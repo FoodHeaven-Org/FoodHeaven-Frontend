34a90abb470f99fcbd7ebbd77242c78526b2372e
luis <luispierorodriguez@gmail.com>
2026-05-16 11:13:59 -0500

    fix: show language switcher on auth pages

--- src/security/interfaces/presentation/register/register-view.page.vue
+++ src/security/interfaces/presentation/register/register-view.page.vue
@@ -1,10 +1,25 @@
 <script setup>
 import FormComponent from '@/security/interfaces/presentation/register/Form.component.vue'
+import LanguageSwitcher from '@/shared/interfaces/presentation/language-switcher.component.vue'
 </script>
 
 <template>
-  <FormComponent />
+  <div class="auth-page">
+    <LanguageSwitcher class="auth-language-switcher" />
+    <FormComponent />
+  </div>
 </template>
 
 <style scoped>
+.auth-page {
+  position: relative;
+  min-height: 100vh;
+}
+
+.auth-language-switcher {
+  position: fixed;
+  top: 1rem;
+  right: 1rem;
+  z-index: 10;
+}
 </style>
